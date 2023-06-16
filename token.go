1a3fd59d95e039bc12780630a93149635f9338c1
Luis Eduardo M <luiseduardomc019@gmail.com>
2023-06-15 22:44:34 -0300

    feat: token/api initial funcs

--- token.go
+++ token.go
@@ -10,20 +10,28 @@ import (
 	"golang.org/x/oauth2"
 )
 
-func Token() (oauth2.TokenSource, error) {
+func apiKey() string {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	ghKey := os.Getenv("GITHUB_API_KEY")
+	githubKey := os.Getenv("GITHUB_API_KEY")
+
+	return githubKey
+}
+
+func Token() (oauth2.TokenSource, error) {
+	githubKey := apiKey()
 
 	tokenSource := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: ghKey},
+		&oauth2.Token{AccessToken: githubKey},
 	)
 
 	return tokenSource, nil
 }
 
-func TokenClient(ctx context.Context, ts oauth2.TokenSource) *http.Client {
+func TokenClient(ts oauth2.TokenSource) *http.Client {
+	ctx := context.Background()
+
 	return oauth2.NewClient(ctx, ts)
 }
