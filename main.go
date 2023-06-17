cf649339f1a61f882eb4fded6e39fdc152f112c3
Luis Eduardo M <luiseduardomc019@gmail.com>
2023-06-16 22:40:35 -0300

    refactor: removed application logic

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-)
-
 func main() {
-	ctx := context.Background()
-
-	// TokenSource
-	ts, err := Token()
-	if err != nil {
-		log.Println(err)
-	}
-
-	// TokenClient
-	tc := TokenClient(ctx, ts)
-	client := GithubClient(tc)
-
-	var repoName string
-	fmt.Scanf("%s", &repoName)
-
-	// CreateRepository(ctx, repoName, true, client)
-	SearchRepository(ctx, client, "luisedmc", repoName)
+	StartGHCMD()
 }
