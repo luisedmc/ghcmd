609a42d1f812949e92ab98a1c44cbde2c467bd75
Luis Eduardo M <luiseduardomc019@gmail.com>
2023-06-17 22:33:12 -0300

    refactor: init ghcmd tui

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
 func main() {
-	StartGHCMD()
+	p := tea.NewProgram(StartGHCMD())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
