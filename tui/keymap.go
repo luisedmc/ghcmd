4e16701439790e084cf9669d19a60bd608f3ebcf
Luis Eduardo M <luiseduardomc019@gmail.com>
2023-06-21 20:20:33 -0300

    moved to package tui

---
+++ tui/keymap.go
@@ -0,0 +1,17 @@
+package tui
+
+import "github.com/charmbracelet/bubbles/key"
+
+type KeyMap struct {
+	Up   key.Binding
+	Down key.Binding
+	Quit key.Binding
+}
+
+func (k KeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Up, k.Down, k.Quit}
+}
+
+func (k KeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{k.ShortHelp()}
+}
