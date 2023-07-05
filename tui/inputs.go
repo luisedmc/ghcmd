a64c551cb7ef72e5d9919aa8114b1868ba68996f
Luis Eduardo M <luiseduardomc019@gmail.com>
2023-07-05 19:54:23 -0300

    feat: create repo inputs

--- tui/inputs.go
+++ tui/inputs.go
@@ -5,7 +5,7 @@ import "github.com/charmbracelet/bubbles/textinput"
 // TokenInput creates a text input for the token
 func TokenInput() textinput.Model {
 	ti := textinput.New()
-	ti.Placeholder = "you can paste it"
+	ti.Placeholder = "your api token (you can paste it)"
 	ti.Focus()
 	ti.CharLimit = 40
 
@@ -24,4 +24,18 @@ func SearchInputs() []textinput.Model {
 	ri.CharLimit = 100
 
 	return []textinput.Model{ui, ri}
+}
+
+// CreateInputs creates two text inputs for the repository name and if it's private
+func CreateInputs() []textinput.Model {
+	rn := textinput.New()
+	rn.Placeholder = "repository name"
+	rn.Focus()
+	rn.CharLimit = 100
+
+	p := textinput.New()
+	p.Placeholder = "private (y/n)"
+	p.CharLimit = 1
+
+	return []textinput.Model{rn, p}
 }
