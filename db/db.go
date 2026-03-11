2e734638ae2a066a82128769d98020ba66f98265
Luis Eduardo M <luiseduardomc019@gmail.com>
2026-03-10 22:15:47 -0300

    removed unnecessary functions + using dynamic dir to create db data folder

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
@@ -10,50 +11,24 @@ type Database struct {
 	Conn *leveldb.DB
 }
 
-func createDB(dbPath string) error {
-	db, err := leveldb.OpenFile(dbPath, nil)
-	if err != nil {
-		return err
-	}
-	// log.Println("Database created successfully.")
-	defer db.Close()
-
-	return nil
-}
-
-// OpenDB opens a database file
+// OpenDB opens (or creates) the database at the user's config directory
 func OpenDB() (*Database, error) {
-	_, err := os.Stat("./db/data")
+	configDir, err := os.UserConfigDir()
 	if err != nil {
-		if os.IsNotExist(err) {
-			err = createDB("./db/data")
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			return nil, err
-		}
+		return nil, err
+	}
+	dbPath := filepath.Join(configDir, "ghcmd", "data")
+
+	if err := os.MkdirAll(filepath.Dir(dbPath), 0755); err != nil {
+		return nil, err
 	}
 
-	db, err := leveldb.OpenFile("./db/data", nil)
+	db, err := leveldb.OpenFile(dbPath, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	d := &Database{db}
-	// log.Println("Database opened successfully.")
-
-	return d, nil
-}
-
-// PutToken stores the token in the database
-func (d *Database) PutToken(db *leveldb.DB, token string) error {
-	err := db.Put([]byte("gh_token"), []byte(token), nil)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return &Database{db}, nil
 }
 
 // GetToken retrieves the token from the database
